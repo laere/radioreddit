a46abcc2e587d34677a3434fd62397f86b04ce5b
Zack Bostian <bostianz@gmail.com>
2016-03-16 12:11:19 -0400

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 
 const Navbar = () => {
   return (
-    <div className="mainNavbar">
-
-    </div>
+    <nav role="navigation">
+      <div className="mainNavbar">
+        <div className="navItem"><span>About</span></div>
+        <div className="navItem"><span>Channels</span></div>
+        <div className="navItem"> <span>Library</span></div>
+        <div className="navItem"><span>Hot</span></div>
+        <div className="navItem"><span>Upload</span></div>
+      </div>
+    </nav>
   );
 };
 
