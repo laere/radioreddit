c9f0bd7ec909e72c918292739a74a402de4f1c9f
Zack Bostian <bostianz@gmail.com>
2016-03-16 18:51:44 -0400

    condensed navbar css  a bit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,21 +6,19 @@ import NavLink from './NavLink';
 const Navbar = () => {
   return (
     <header>
-      <nav role="navigation row">
-        <img src="http://radioreddit.com/images/logo.svg" alt="radio reddit" width="300px"/>
-        <div className="mainNavbar">
-          <NavLink header="About" />
-          <NavLink header="Channels" />
-          <NavLink header="Hot" />
-          <NavLink header="Library" />
-          <NavLink header="Upload" />
-        </div>
-        <div className="mainNavbarIcons">
-          <i className="fa fa-search fa-lg"/>
-          <i className="fa fa-user fa-lg"/>
-          <button>Log In</button>
-        </div>
+      <img src="http://radioreddit.com/images/logo.svg" alt="radio reddit" width="300px"/>
+      <nav role="navigation" className="mainNavbar">
+        <NavLink header="About" />
+        <NavLink header="Channels" />
+        <NavLink header="Hot" />
+        <NavLink header="Library" />
+        <NavLink header="Upload" />
       </nav>
+      <div className="mainNavbarIcons">
+        <i className="fa fa-search fa-lg"/>
+        <i className="fa fa-user fa-lg"/>
+        <button>Log In</button>
+      </div>
     </header>
   );
 };
